d53504432b66c56adcce459c1277f8ccb7a62196
liaa244 <147677241+liaa244@users.noreply.github.com>
2025-04-16 04:41:44 -0700

    Add files via upload

---
+++ pre-praktikum/pre 2.sql
@@ -0,0 +1,21 @@
+-- NAMA : aulia yulianti 
+-- NIM : 23241089
+-- KELAS : c 
+
+-- Melihat database yang ada 
+show databases 
+
+-- Melihat database
+USE mysql;
+
+-- Melihat tabel dalam mysql 
+SHOW TABLES;
+
+--Mendiskripsikan sebuah tabel yang yang dipilih ex, tabel, "user"
+DESCRIBE user 
+
+-- melihat isi tabel user, khusus untuk kolom Host, User, dan Password 
+SELECT host, user, password, FROM user;
+
+
+
